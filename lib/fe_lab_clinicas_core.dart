c7d3eb61ade45cf0710c872b8371ab11301a5c08
IgorBVieira <igorvieira.org@gmail.com>
2024-01-30 17:02:54 -0300

    export the classes

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -4,3 +4,7 @@ library fe_lab_clinicas_core;
 // Classes privadas em SRC
 
 
+export './src/fp/either.dart';
+export './src/fp/nil.dart';
+export './src/fp/unit.dart';
+export './src/helpers/messages.dart';
